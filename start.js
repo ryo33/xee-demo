19a80a274d564a3e9d2677cc74a0d2a1dbaf5fcd
ryo33 <ryo12redstone@gmail.com>
2014-11-02 06:16:27 -0800

    move requirement files to requirement folder

--- start.js
+++ start.js
@@ -2,6 +2,8 @@ $("title").append(title);
 $("#header").append(header);
 $("#footer").append(footer);
 
+//TODO access setting.php and get settings
+
 var token = get("token")
 if(token){
     refresh();
